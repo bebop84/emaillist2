647825b9f172d900ebacb769f9ca21aa9a09ffa9
bit-user <bit-user@222.106.22.80>
2015-10-12 14:46:36 +0900

    Action Factory, Action 추가

--- src/com/bit2015/emaillist/servlet/EmailListServlet.java
+++ src/com/bit2015/emaillist/servlet/EmailListServlet.java
@@ -1,17 +1,16 @@
 package com.bit2015.emaillist.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.bit2015.emaillist.dao.EmailListDao;
-import com.bit2015.emaillist.vo.EmailListVo;
+import com.bit2015.emaillist.web.action.EmailListActionFactory;
+import com.bit2015.web.action.Action;
+import com.bit2015.web.action.ActionFactory;
 
 @WebServlet("/el")
 public class EmailListServlet extends HttpServlet {
@@ -23,44 +22,11 @@ public class EmailListServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding( "UTF-8" );
-		
-		String a = request.getParameter( "a" );
-		
-		if( "form".equals( a ) ) {
-			
-			forwarding( request, response, "/views/form_emaillist.jsp");
-			
-		} else if( "add".equals( a ) ) {
-			String firstName = request.getParameter( "fn" );
-			String lastName = request.getParameter( "ln" );
-			String email = request.getParameter( "email" );
-			
-			EmailListVo vo = new EmailListVo();
-			vo.setFirstName(firstName);
-			vo.setLastName(lastName);
-			vo.setEmail(email);
-			
-			EmailListDao dao = new EmailListDao();
-			dao.insert(vo);
-			
-			response.sendRedirect( "/emaillist2/el?a=show" );
-		} else {
-			// default action
-			EmailListDao dao = new EmailListDao();
-			List<EmailListVo> list = dao.getList();
-			
-			request.setAttribute( "list", list );
-			forwarding( request, response, "/views/show_emaillist.jsp" );
-		}
+		String actionName = request.getParameter( "a" );
+		ActionFactory actionFactory = new EmailListActionFactory();
+		Action action = actionFactory.getAction( actionName );
+		action.execute(request, response);
 	}
 	
-	private void forwarding(
-		HttpServletRequest request,
-		HttpServletResponse response,
-		String path ) throws ServletException, IOException {
-		
-		RequestDispatcher requestDispatcher =
-				request.getRequestDispatcher( path );
-		requestDispatcher.forward( request, response );
-	}
+
 }
